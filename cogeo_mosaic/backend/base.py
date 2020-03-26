0d4d98e54835e76595eb187e18b418f5721e32fa
Kyle Barron <barronk@mit.edu>
2020-03-25 21:57:47 -0600

    BaseBackend idea

---
+++ cogeo_mosaic/backend/base.py
@@ -0,0 +1,25 @@
+"""cogeo_mosaic.backend.base: base Backend class."""
+
+import abc
+
+
+class BackendBase(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def metadata(self, *args, **kwargs):
+        """Retrieve MosaicJSON metadata."""
+
+    @abc.abstractmethod
+    def tile(self, *args, **kwargs):
+        """Retrieve assets for tile."""
+
+    @abc.abstractmethod
+    def point(self, *args, **kwargs):
+        """Retrieve assets for point."""
+
+    @abc.abstractmethod
+    def create(self, *args, **kwargs):
+        """Upload new MosaicJSON to backend."""
+
+    @abc.abstractmethod
+    def update(self, *args, **kwargs):
+        """Update existing MosaicJSON on backend."""
