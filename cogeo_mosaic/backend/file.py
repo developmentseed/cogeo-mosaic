8444b8df5d1790f60b3b70942a0b34718470bd4a
Kyle Barron <barronk@mit.edu>
2020-03-26 20:32:29 -0600

    Add file upload method

--- cogeo_mosaic/backend/file.py
+++ cogeo_mosaic/backend/file.py
@@ -6,7 +6,7 @@ from boto3.session import Session as boto3_session
 
 from cogeo_mosaic.backend.base import BaseBackend
 from cogeo_mosaic.backend.utils import get_assets_from_json
-from cogeo_mosaic.utils import _decompress_gz
+from cogeo_mosaic.utils import _compress_gz_json, _decompress_gz
 
 
 class FileBackend(BaseBackend):
@@ -31,6 +31,11 @@ class FileBackend(BaseBackend):
         return get_assets_from_json(
             self.mosaic_def["tiles"], self.quadkey_zoom, tile.x, tile.y, tile.z
         )
+
+    def upload(self, mosaic: Dict):
+        path = f"mosaics/{self.mosaicid}.json.gz"
+        with open(path, "wb") as f:
+            f.write(_compress_gz_json(mosaic))
 
     @functools.lru_cache(maxsize=512)
     def fetch_mosaic_definition(self, path: str) -> Dict:
