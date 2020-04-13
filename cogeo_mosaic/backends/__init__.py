6f8eda86e9979347216b9b638e49be820aff8a3c
Kyle Barron <barronk@mit.edu>
2020-04-12 22:31:24 -0600

    Rename dynamo.py -> dynamodb.py

--- cogeo_mosaic/backends/__init__.py
+++ cogeo_mosaic/backends/__init__.py
@@ -1,5 +1,5 @@
 from .auto import auto_backend
-from .dynamo import DynamoDBBackend
+from .dynamodb import DynamoDBBackend
 from .file import FileBackend
 from .http import HttpBackend
 from .s3 import S3Backend
