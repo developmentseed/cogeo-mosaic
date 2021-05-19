7e49753862e78c0ff731bf350a7b69e4fc297b4f
vincentsarago <vincent.sarago@gmail.com>
2021-05-19 16:03:04 -0400

    advertise python 3.8 and 3.9 support

--- setup.py
+++ setup.py
@@ -39,6 +39,8 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering :: GIS",
     ],
     keywords="COG Mosaic GIS",
