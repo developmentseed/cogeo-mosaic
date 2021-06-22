53409dbf1f6858393f017c5a1db205e58cb44ee6
vincentsarago <vincent.sarago@gmail.com>
2021-06-22 14:53:59 +0200

    Bump version: 3.0.0 → 3.0.1

--- cogeo_mosaic/__init__.py
+++ cogeo_mosaic/__init__.py
@@ -1,3 +1,3 @@
 """Cogeo_mosaic."""
 
-__version__ = "3.0.0"
+__version__ = "3.0.1"
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ extra_reqs = {
 
 setup(
     name="cogeo-mosaic",
-    version="3.0.0",
+    version="3.0.1",
     description=u"Create mosaicJSON.",
     long_description=readme,
     long_description_content_type="text/markdown",
