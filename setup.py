f0217c23a3e16d5ce7bd60e9a42500712a598b75
vincentsarago <vincent.sarago@gmail.com>
2022-10-21 14:55:49 +0200

    add fake setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+"""Fake cogeo-mosaic setup.py for github."""
+import sys
+
+from setuptools import setup
+
+sys.stderr.write(
+    """
+===============================
+Unsupported installation method
+===============================
+cogeo-mosaic no longer supports installation with `python setup.py install`.
+Please use `python -m pip install .` instead.
+"""
+)
+sys.exit(1)
+
+
+# The below code will never execute, however GitHub is particularly
+# picky about where it finds Python packaging metadata.
+# See: https://github.com/github/feedback/discussions/6456
+#
+# To be removed once GitHub catches up.
+
+setup(
+    name="cogeo-mosaic",
+    install_requires=[
+        "attrs",
+        "morecantile>=3.1,<4.0",
+        "pygeos>=0.10",
+        "pydantic",
+        "httpx",
+        "rasterio",
+        "rio-tiler>=4.0.0a0,<5.0",
+        "cachetools",
+        "supermercado",
+    ],
+)
